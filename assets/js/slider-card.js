6bbe20bed421dba6481b2e819e7947fcb8b467bd
Slem7415t <xalexsemx@yandex.ru>
2024-01-29 13:06:19 +0300

    version-1.3

---
+++ assets/js/slider-card.js
@@ -0,0 +1,156 @@
+export function arrSliderCard() {
+    const sliderCard =  [
+        {
+            "id": 1,
+            "images" : "assets/images/catalog/gibkaya-vstavka-danfoss-zkv-082x9036-ru16-du125-902209788-1000x1000 1.jpg",
+            "title": 'title',
+            "description": 'Гибкая вставка резиновая (вибровставка) фланцевая ДУ 125 Ру 10/16'
+        },
+        {
+            "id": 2,
+            "images" : "assets/images/catalog/gibkaya-vstavka-danfoss-zkv-082x9036-ru16-du125-902209788-1000x1000 1.jpg",
+            "title": 'title',
+            "description": 'Гибкая вставка резиновая (вибровставка) фланцевая ДУ 125 Ру 10/16'
+        },
+        {
+            "id": 3,
+            "images" : "assets/images/catalog/gibkaya-vstavka-danfoss-zkv-082x9036-ru16-du125-902209788-1000x1000 1.jpg",
+            "title": 'title',
+            "description": 'Гибкая вставка резиновая (вибровставка) фланцевая ДУ 125 Ру 10/16'
+        },
+        {
+            "id": 4,
+            "images" : "assets/images/catalog/ZERN-2000_2018 1.jpg",
+            "title": 'title',
+            "description": 'Гибкая вставка резиновая (вибровставка) фланцевая ДУ 125 Ру 10/16'
+        },
+        {
+            "id": 5,
+            "images" : "assets/images/catalog/gibkaya-vstavka-danfoss-zkv-082x9036-ru16-du125-902209788-1000x1000 1.jpg",
+            "title": 'title',
+            "description": 'Гибкая вставка резиновая (вибровставка) фланцевая ДУ 125 Ру 10/16'
+        },
+        {
+            "id": 6,
+            "images" : "assets/images/catalog/ZERN-2000_2018 1.jpg",
+            "title": 'title',
+            "description": 'Гибкая вставка резиновая (вибровставка) фланцевая ДУ 125 Ру 10/16'
+        },
+        {
+            "id": 7,
+            "images" : "assets/images/catalog/gibkaya-vstavka-danfoss-zkv-082x9036-ru16-du125-902209788-1000x1000 1.jpg",
+            "title": 'title',
+            "description": 'Гибкая вставка резиновая (вибровставка) фланцевая ДУ 125 Ру 10/16'
+        },
+        {
+            "id": 8,
+            "images" : "assets/images/catalog/gibkaya-vstavka-danfoss-zkv-082x9036-ru16-du125-902209788-1000x1000 1.jpg",
+            "title": 'title',
+            "description": 'Гибкая вставка резиновая (вибровставка) фланцевая ДУ 125 Ру 10/16'
+        },
+        // {
+        //     "id": 9,
+        //     "images" : "assets/images/catalog/gibkaya-vstavka-danfoss-zkv-082x9036-ru16-du125-902209788-1000x1000 1.jpg",
+        //     "title": 'title',
+        //     "description": 'Гибкая вставка резиновая (вибровставка) фланцевая ДУ 125 Ру 10/16'
+        // },
+        // {
+        //     "id": 10,
+        //     "images" : "assets/images/catalog/gibkaya-vstavka-danfoss-zkv-082x9036-ru16-du125-902209788-1000x1000 1.jpg",
+        //     "title": 'title',
+        //     "description": 'Гибкая вставка резиновая (вибровставка) фланцевая ДУ 125 Ру 10/16'
+        // },
+        // {
+        //     "id": 11,
+        //     "images" : "assets/images/catalog/ZERN-2000_2018 1.jpg",
+        //     "title": 'title',
+        //     "description": 'Гибкая вставка резиновая (вибровставка) фланцевая ДУ 125 Ру 10/16'
+        // },
+        // {
+        //     "id": 12,
+        //     "images" : "assets/images/catalog/gibkaya-vstavka-danfoss-zkv-082x9036-ru16-du125-902209788-1000x1000 1.jpg",
+        //     "title": 'title',
+        //     "description": 'Гибкая вставка резиновая (вибровставка) фланцевая ДУ 125 Ру 10/16'
+        // },
+        // {
+        //     "id": 13,
+        //     "images" : "assets/images/catalog/gibkaya-vstavka-danfoss-zkv-082x9036-ru16-du125-902209788-1000x1000 1.jpg",
+        //     "title": 'title',
+        //     "description": 'Гибкая вставка резиновая (вибровставка) фланцевая ДУ 125 Ру 10/16'
+        // },
+        // {
+        //     "id": 14,
+        //     "images" : "assets/images/catalog/gibkaya-vstavka-danfoss-zkv-082x9036-ru16-du125-902209788-1000x1000 1.jpg",
+        //     "title": 'title',
+        //     "description": 'Гибкая вставка резиновая (вибровставка) фланцевая ДУ 125 Ру 10/16'
+        // },
+        // {
+        //     "id": 15,
+        //     "images" : "assets/images/catalog/gibkaya-vstavka-danfoss-zkv-082x9036-ru16-du125-902209788-1000x1000 1.jpg",
+        //     "title": 'title',
+        //     "description": 'Гибкая вставка резиновая (вибровставка) фланцевая ДУ 125 Ру 10/16'
+        // },
+        // {
+        //     "id": 16,
+        //     "images" : "assets/images/catalog/gibkaya-vstavka-danfoss-zkv-082x9036-ru16-du125-902209788-1000x1000 1.jpg",
+        //     "title": 'title',
+        //     "description": 'Гибкая вставка резиновая (вибровставка) фланцевая ДУ 125 Ру 10/16'
+        // },
+        // {
+        //     "id": 17,
+        //     "images" : "assets/images/catalog/gibkaya-vstavka-danfoss-zkv-082x9036-ru16-du125-902209788-1000x1000 1.jpg",
+        //     "title": 'title',
+        //     "description": 'Гибкая вставка резиновая (вибровставка) фланцевая ДУ 125 Ру 10/16'
+        // },
+        // {
+        //     "id": 18,
+        //     "images" : "assets/images/catalog/gibkaya-vstavka-danfoss-zkv-082x9036-ru16-du125-902209788-1000x1000 1.jpg",
+        //     "title": 'title',
+        //     "description": 'Гибкая вставка резиновая (вибровставка) фланцевая ДУ 125 Ру 10/16'
+        // },
+        // {
+        //     "id": 19,
+        //     "images" : "assets/images/catalog/gibkaya-vstavka-danfoss-zkv-082x9036-ru16-du125-902209788-1000x1000 1.jpg",
+        //     "title": 'title',
+        //     "description": 'Гибкая вставка резиновая (вибровставка) фланцевая ДУ 125 Ру 10/16'
+        // },
+        // {
+        //     "id": 20,
+        //     "images" : "assets/images/catalog/ZERN-2000_2018 1.jpg",
+        //     "title": 'title',
+        //     "description": 'Гибкая вставка резиновая (вибровставка) фланцевая ДУ 125 Ру 10/16'
+        // },
+        // {
+        //     "id": 21,
+        //     "images" : "assets/images/catalog/gibkaya-vstavka-danfoss-zkv-082x9036-ru16-du125-902209788-1000x1000 1.jpg",
+        //     "title": 'title',
+        //     "description": 'Гибкая вставка резиновая (вибровставка) фланцевая ДУ 125 Ру 10/16'
+        // },
+        // {
+        //     "id": 22,
+        //     "images" : "assets/images/catalog/gibkaya-vstavka-danfoss-zkv-082x9036-ru16-du125-902209788-1000x1000 1.jpg",
+        //     "title": 'title',
+        //     "description": 'Гибкая вставка резиновая (вибровставка) фланцевая ДУ 125 Ру 10/16'
+        // },
+        // {
+        //     "id": 23,
+        //     "images" : "assets/images/catalog/ZERN-2000_2018 1.jpg",
+        //     "title": 'title',
+        //     "description": 'Гибкая вставка резиновая (вибровставка) фланцевая ДУ 125 Ру 10/16'
+        // },
+        // {
+        //     "id": 24,
+        //     "images" : "assets/images/catalog/gibkaya-vstavka-danfoss-zkv-082x9036-ru16-du125-902209788-1000x1000 1.jpg",
+        //     "title": 'title',
+        //     "description": 'Гибкая вставка резиновая (вибровставка) фланцевая ДУ 125 Ру 10/16'
+        // },
+        // {
+        //     "id": 25,
+        //     "images" : "assets/images/catalog/gibkaya-vstavka-danfoss-zkv-082x9036-ru16-du125-902209788-1000x1000 1.jpg",
+        //     "title": 'title',
+        //     "description": 'Гибкая вставка резиновая (вибровставка) фланцевая ДУ 125 Ру 10/16'
+        // }
+    ];
+
+    return sliderCard;
+}
